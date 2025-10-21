2142e98877e22443aaee28885ca427c1ab5f6996
Thuat <nnt020807@gmail.com>
2025-10-21 23:48:49 +0700

    refactoring code, add caption, add background

--- index.py
+++ index.py
@@ -1,22 +1,38 @@
-import pygame 
+import pygame
+import os 
 
-pygame.init()
+def main():
+    pygame.init()
 
-SCREEN = pygame.display.set_mode([1000, 600])
+    screen_width = 1000
+    screen_height = 600
+    screen = pygame.display.set_mode([screen_width, screen_height], pygame.RESIZABLE)
+    pygame.display.set_caption("MUMMY MAZE")
 
-running = True
-while running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    SCREEN.fill((255,255,255))
-
-    pygame.display.flip()
+    # Load and scale background image
+    background_image = pygame.image.load(os.path.join('Assets', 'Images', 'background.jpg')).convert()
+    background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
 
 
-pygame.quit()
+    running = True   # Main loop flag
+
+    while running:
+        
+        for event in pygame.event.get():   # Event handling
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.blit(background_image, (0,0))   # Draw background
+
+        # Update the display
+        pygame.display.flip()
+        pygame.display.update()
+
+    pygame.quit() # Quit Pygame
+
+
+if __name__ == "__main__":
+    main()
 
 
 
