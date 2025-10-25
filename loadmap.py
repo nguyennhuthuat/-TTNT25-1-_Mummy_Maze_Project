d830137c4257b5c400f797068974f51fa388b156
Thuat <nnt020807@gmail.com>
2025-10-25 17:13:09 +0700

    clean code, add feature draw map (not include draw stair)

--- loadmap.py
+++ loadmap.py
@@ -1,65 +1,53 @@
 import pygame
 import os
 
-
-
-
 pygame.init()
-
 screen_width = 1000
 screen_height = 600
-TILE_SIZE = 70 # Size of each tile in the grid
-backdrop_width = 575 # Set width  size for the backdrop
-backdrop_height = 558 # Set height size for the backdrop
-
 screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
 pygame.display.set_caption("Load Map Example")
 clock = pygame.time.Clock() # For controlling frame rate
 
-
-# Load background and game square images
-backdrop = pygame.image.load(os.path.join("assets","image", "backdrop.jpg")).convert()
-
- 
-
-backdrop = pygame.transform.scale(backdrop, (backdrop_width, backdrop_height))
-game_square = pygame.image.load(os.path.join("assets","image", "floor6.jpg")).convert_alpha()
-game_square = pygame.transform.scale(game_square, (TILE_SIZE*6, TILE_SIZE*6)) 
-
 class Mum_Map:  
     
     def __init__(self):
-        self.database = {
-            't': 'top_wall_tile.jpg',
-            'b': 'bottom_wall_tile.jpg',
-            'l': 'left_wall_tile.jpg',
-            'r': 'right_wall_tile.jpg',
-            'tr': 'top_right_wall_tile.jpg',
-            'tl': 'top_left_wall_tile.jpg',
-            'br': 'bottom_right_wall_tile.jpg',
-            'bl': 'bottom_left_wall_tile.jpg',
-            'l*': 'left_t_wall_tile.jpg',
-            'r*': 'right_t_wall_tile.jpg',
-            't*': 'top_t_wall_tile.jpg',
-            'b*': 'bottom_t_wall_tile.jpg'}
+        self.database = { # dictionary to link tile id to drawing function
+            't': 'draw_top_wall_tile',
+            'b': 'draw_bottom_wall_tile',
+            'l': 'draw_left_wall_tile',
+            'r': 'draw_right_wall_tile',
+            'tr': 'draw_top_right_wall_tile',
+            'tl': 'draw_top_left_wall_tile',
+            'br': 'draw_bottom_right_wall_tile',
+            'bl': 'draw_bottom_left_wall_tile',
+            'l*': 'draw_left_t_wall_tile',
+            'r*': 'draw_right_t_wall_tile',
+            't*': 'draw_top_t_wall_tile',
+            'b*': 'draw_bottom_t_wall_tile'}
         
         self.map_data = [['', '', '', '', '', ''],
-                         ['r ', '', 'tl', '', '', ''],
+                         ['r', '', 'tl', '', '', ''],
+                         ['', '', 'bl', '', '', 't'],
                          ['', '', '', '', '', ''],
-                         ['', '', '', '', '', ''],
-                         ['', '', '', '', '', ''],
-                         ['', '', '', '', '', '']]
+                         ['', 'cc', '', 'tl', 't*', ''],
+                         ['', '', '', '', 'r', '']]
         
-        self.load_image()
+        
+        self.TILE_SIZE = 70 # Size of each tile in the grid
+        self.backdrop_width = 575 # Set width  size for the backdrop
+        self.backdrop_height = 558 # Set height size for the backdrop
+        self.margin_left = 78 + (screen_width-self.backdrop_width)//2 # distance from left edge to game square left edge
+        self.margin_top = 93 + (screen_height-self.backdrop_height)//2 # distance from top edge to game square top edge
+        # Load background and game square images
+        self.backdrop = pygame.image.load(os.path.join("assets","image", "backdrop.png")).convert()
+        self.backdrop = pygame.transform.scale(self.backdrop, (self.backdrop_width, self.backdrop_height))
+        self.game_square = pygame.image.load(os.path.join("assets","image", "floor6.png")).convert_alpha()
+        self.game_square = pygame.transform.scale(self.game_square, (self.TILE_SIZE*6, self.TILE_SIZE*6)) 
 
-        self.margin_left = 78 + (screen_width-backdrop_width)//2 # distance from left edge to game square left edge
-        self.margin_top = 93 + (screen_height-backdrop_height)//2 # distance from top edge to game square top edge
-    def load_image_(self, id):
-        img_path = os.path.join("assets","image", self.database[id])
-        img = pygame.image.load(img_path).convert_alpha()
-        return img
+
+        self.load_tiles()
     
-    def load_image(self):
+    def load_tiles(self):
         #seperate wall image to cut from walls6.png 
         area_surface = pygame.image.load(os.path.join("assets","image", 'walls6.png')).convert_alpha()
 
@@ -67,43 +55,108 @@ class Mum_Map:
         self.down_standing_wall = pygame.transform.scale(area_surface.subsurface(area_to_cut), (14,91))
 
         area_to_cut = pygame.Rect(12, 0, 72,18)
-        self.lying_wall = pygame.transform.scale(area_surface.subsurface(area_to_cut), (86,21))
+        self.lying_wall = pygame.transform.scale(area_surface.subsurface(area_to_cut), (84,21))
 
         area_to_cut = pygame.Rect(84, 0, 12,74)
-        self.up_standing_wall = pygame.transform.scale(area_surface.subsurface(area_to_cut), (14,87)) ### bottom col wall = 14x91, rol wall = 86x21, top col wall = 14x87
-
-        return 0
-
-
-    def draw_map(self, screen, map_data):
-        for row_index, row in enumerate(map_data):
-            for col_index, tile_id in enumerate(row):
-                if tile_id.strip():  # Only draw if tile_id is not empty
-                    img = self.load_image(tile_id)
-                    x = col_index * TILE_SIZE
-                    y = row_index * TILE_SIZE
-                    screen.blit(img, (x, y))
+        self.up_standing_wall = pygame.transform.scale(area_surface.subsurface(area_to_cut), (14,87)) ### bottom col wall = 14x91, rol wall = 84x21, top col wall = 14x87
         
+        #seperate stair image to cut from stairs.png
+        area_stair_surface = pygame.image.load(os.path.join("assets","image", 'stairs.png')).convert_alpha()
+        
+        area_to_cut = pygame.Rect(2, 0, 54, 66)
+        self.bottom_stair = pygame.transform.scale(area_stair_surface.subsurface(area_to_cut), (63,77))
 
+        area_to_cut = pygame.Rect(60, 0, 54, 66)
+        self.left_stair = pygame.transform.scale(area_stair_surface.subsurface(area_to_cut), (63,77))
+
+        area_to_cut = pygame.Rect(114, 0, 54, 36)
+        self.top_stair = pygame.transform.scale(area_stair_surface.subsurface(area_to_cut), (63,42))
+
+        area_to_cut = pygame.Rect(170, 0, 54, 66)
+        self.right_stair = pygame.transform.scale(area_stair_surface.subsurface(area_to_cut), (63,77))
+        return 0
+    
+    def draw_top_wall_tile(self, screen, x, y): # x,y: the cell at row x, column y in a square table/board
+        x -= 1
+        y -= 1
+        screen.blit(self.lying_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y - 14))  
+    def draw_bottom_wall_tile(self, screen, x, y): # x,y: the cell at row x, column y in a square table/board
+        x -= 1
+        y -= 1
+        screen.blit(self.lying_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y + self.TILE_SIZE - 14))
+    def draw_left_wall_tile(self, screen, x, y): # x,y: the cell at row x, column y in a square table/board
+        x -= 1
+        y -= 1
+        screen.blit(self.down_standing_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y - 14))
+    def draw_right_wall_tile(self, screen, x, y): # x,y: the cell at row x, column y in a square table/board
+        x -= 1
+        y -= 1
+        screen.blit(self.down_standing_wall, (self.margin_left + self.TILE_SIZE*x + self.TILE_SIZE - 3, self.margin_top + self.TILE_SIZE*y - 14))
+    def draw_bottom_left_wall_tile(self, screen, x, y): # x,y: the cell at row x, column y in a square table/board
+        x -= 1
+        y -= 1
+        screen.blit(self.up_standing_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y - 14))
+        screen.blit(self.lying_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y + self.TILE_SIZE - 14))
+    def draw_top_left_wall_tile(self, screen, x, y): # x,y: the cell at row x, column y in a square table/board
+        x -= 1
+        y -= 1
+        screen.blit(self.lying_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y - 14))
+        screen.blit(self.down_standing_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y - 14))
+    def draw_bottom_right_wall_tile(self, screen, x, y): # x,y: the cell at row x, column y in a square table/board
+        x -= 1
+        y -= 1
+        screen.blit(self.up_standing_wall, (self.margin_left + self.TILE_SIZE*x + self.TILE_SIZE - 3, self.margin_top + self.TILE_SIZE*y - 14))
+        screen.blit(self.lying_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y + self.TILE_SIZE - 14))
+    def draw_top_right_wall_tile(self, screen, x, y): # x,y: the cell at row x, column y in a square table/board
+        x -= 1
+        y -= 1
+        screen.blit(self.lying_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y - 14))
+        screen.blit(self.down_standing_wall, (self.margin_left + self.TILE_SIZE*x + self.TILE_SIZE - 3, self.margin_top + self.TILE_SIZE*y - 14))
+    def draw_left_t_wall_tile(self, screen, x, y): # x,y: the cell at row x, column y in a square table/board
+        x -= 1
+        y -= 1
+        screen.blit(self.lying_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y - 14))
+        screen.blit(self.up_standing_wall, (self.margin_left + self.TILE_SIZE*x + self.TILE_SIZE - 3, self.margin_top + self.TILE_SIZE*y - 14))
+        screen.blit(self.lying_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y + self.TILE_SIZE - 14))
+    def draw_right_t_wall_tile(self, screen, x, y): # x,y: the cell at row x, column y in a square table/board
+        x -= 1
+        y -= 1
+        screen.blit(self.lying_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y - 14))
+        screen.blit(self.down_standing_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y - 14))
+        screen.blit(self.lying_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y + self.TILE_SIZE - 14))
+    def draw_top_t_wall_tile(self, screen, x, y): # x,y: the cell at row x, column y in a square table/board
+        x -= 1
+        y -= 1
+        screen.blit(self.up_standing_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y - 14))
+        screen.blit(self.up_standing_wall, (self.margin_left + self.TILE_SIZE*x + self.TILE_SIZE - 3, self.margin_top + self.TILE_SIZE*y - 14))
+        screen.blit(self.lying_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y + self.TILE_SIZE - 14))
+    def draw_bottom_t_wall_tile(self, screen, x, y): # x,y: the cell at row x, column y in a square table/board
+        x -= 1
+        y -= 1
+        screen.blit(self.lying_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y - 14))
+        screen.blit(self.down_standing_wall, (self.margin_left + self.TILE_SIZE*x - 3, self.margin_top + self.TILE_SIZE*y - 14))
+        screen.blit(self.down_standing_wall, (self.margin_left + self.TILE_SIZE*x + self.TILE_SIZE - 3, self.margin_top + self.TILE_SIZE*y - 14))    
+
+    def draw_map(self, screen):
+        screen.blit(self.backdrop, ((screen_width-self.backdrop_width)//2, (screen_height-self.backdrop_height)//2))
+        screen.blit(self.game_square, ( self.margin_left, self.margin_top)) 
+        for row_index, row in enumerate(self.map_data):
+            for col_index, tile_id in enumerate(row):
+                if tile_id.strip() and (tile_id in self.database.keys()):  # Only draw if tile_id is not empty
+                    getattr(self, self.database[tile_id])(screen, col_index + 1, row_index + 1) # Call the drawing function based on tile_id
+                elif tile_id.strip() and tile_id not in self.database.keys():
+                    print(f"Warning: tile_id '{tile_id}' at ({row_index}, {col_index}) not found in database. Skipping drawing.")
+                    continue
+  
 my_map = Mum_Map()
-
-
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # draw game background and square at center
-    screen.blit(backdrop, ((screen_width-backdrop_width)//2, (screen_height-backdrop_height)//2))
-    screen.blit(game_square, ( my_map.margin_left, my_map.margin_top)) 
-      
-      
-    screen.blit(my_map.up_standing_wall, (my_map.margin_left + TILE_SIZE*4 - 4, my_map.margin_top + TILE_SIZE*4 -87))
-    screen.blit(my_map.lying_wall, (my_map.margin_left + TILE_SIZE*4 - 4, my_map.margin_top + TILE_SIZE*4 - 16))
-    # screen.blit(my_map.down_standing_wall, (my_map.margin_left + TILE_SIZE*4 - 3, my_map.margin_top + TILE_SIZE*4 - 16))
-    screen.blit(my_map.down_standing_wall, (my_map.margin_left + TILE_SIZE*5 -2, my_map.margin_top + TILE_SIZE*4 - 16))
 
+    my_map.draw_map(screen)  
 
     x, y = pygame.mouse.get_pos()
     print(x,y)
