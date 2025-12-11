b744ed6caefb11a82631630c5d4fe94430c14ab1
Thuat <nnt020807@gmail.com>
2025-12-11 17:00:18 +0700

    Change file path

--- index.py
+++ index.py
@@ -1,40 +1,93 @@
+import os
+import time
+from typing import List, Optional, Tuple
+from dataclasses import dataclass
+import random
+
 import pygame
-import os 
 
+from Assets.module.map_collection import maps_collection  # Import a collection of maps
+
+from Assets.module.utils import load_level, get_winning_position
+from Assets.module.map import MummyMazeMapManager
+from Assets.module.explorer import MummyMazePlayerManager
+from Assets.module.zombies import MummyMazeZombieManager
+from Assets.module.scorpion import MummyMazeScorpionManager
+from Assets.module.settings import (
+    SCREEN_WIDTH, SCREEN_HEIGHT, UP, DOWN, LEFT, RIGHT
+)
 def main():
+
+
     pygame.init()
+    pygame.mixer.init() # Initialize the mixer module for sound
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
+    pygame.display.set_caption("Mummy Maze Deluxe")
+    clock = pygame.time.Clock()
 
-    screen_width = 1000
-    screen_height = 600
-    screen = pygame.display.set_mode([screen_width, screen_height], pygame.RESIZABLE)
-    pygame.display.set_caption("MUMMY MAZE")
+    current_level_index = 11
+    map_length, stair_position, map_data, player_start, zombie_starts = load_level(current_level_index)
+    winning_position = get_winning_position(stair_position, map_length)
+    
+    current_tile_size = 480 // map_length  # Dynamically set tile size based on map length
+    MummyMazeMap = MummyMazeMapManager(length = map_length, stair_position = stair_position, map_data = map_data, tile_size = current_tile_size)
+    MummyExplorer = MummyMazePlayerManager(length = map_length, grid_position = player_start, map_data = map_data, tile_size=current_tile_size)
+    MummyZombies = [MummyMazeZombieManager(length = map_length, grid_position = pos, map_data = map_data, tile_size=current_tile_size) for pos in zombie_starts]
 
-    # Load and scale background image
-    background_image = pygame.image.load(os.path.join('Assets', 'images', 'background.png')).convert()
-    background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
-
-
-    running = True   # Main loop flag
-
+    running = True
     while running:
-        
-        for event in pygame.event.get():   # Event handling
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if not MummyZombies[0].movement_list and not MummyExplorer.movement_list:  # Only allow player input if zombies are standing
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        MummyExplorer.update_player_status(UP)
 
-        screen.blit(background_image, (0,0))   # Draw background
+                    elif event.key == pygame.K_DOWN:
+                        MummyExplorer.update_player_status(DOWN)
 
-        # Update the display
+                    elif event.key == pygame.K_LEFT:
+                        MummyExplorer.update_player_status(LEFT)
+
+                    elif event.key == pygame.K_RIGHT:
+                        MummyExplorer.update_player_status(RIGHT)
+
+            
+        MummyMazeMap.draw_map(screen)
+
+        player_turn_completed = MummyExplorer.update_player(screen)
+
+        for zombie in MummyZombies:
+            if player_turn_completed:
+                zombie.zombie_movement(MummyExplorer.grid_position)
+            zombie.update_zombie(screen)
+        MummyMazeMap.draw_walls(screen)
+
+
+        # Check for win condition
+        if winning_position and MummyExplorer.grid_position == winning_position:
+            print("You Won! Loading next level...")
+            current_level_index += 1
+            if current_level_index < len(maps_collection):
+                map_length, stair_position, map_data, player_start, zombie_starts = load_level(current_level_index)
+                winning_position = get_winning_position(stair_position, map_length)
+
+                current_tile_size = 480 // map_length  # Dynamically set tile size based on map length
+                MummyMazeMap = MummyMazeMapManager(length = map_length, stair_position = stair_position, map_data = map_data, tile_size = current_tile_size)
+                MummyExplorer = MummyMazePlayerManager(length = map_length, grid_position = player_start, map_data = map_data, tile_size=current_tile_size)
+                MummyZombies = [MummyMazeZombieManager(length = map_length, grid_position = pos, map_data = map_data, tile_size=current_tile_size) for pos in zombie_starts]
+            else:
+                print("Congratulations! You have completed all levels!")
+                running = False
+
         pygame.display.flip()
-        pygame.display.update()
+        clock.tick(60)
+        time.sleep(0.04)
 
-    pygame.quit() # Quit Pygame
+    pygame.quit()
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
