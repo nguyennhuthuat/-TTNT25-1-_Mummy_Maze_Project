dec67813821892be03724d4dad6e57c5649e1a27
Thuat <nnt020807@gmail.com>
2025-12-19 00:01:07 +0700

    Change test file

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 def pfrint(a = "Hello, World!", b = 42, c = [1, 2, 3]):
     print(a,b,c)
+    print([1,2,3] + [4,5,6])
+    a = [1,2,3]
+    a.append([3,4,5])
+    print(a)
 
 pfrint("Thuat dep trai")
+
